7e543f1db01032b8492c01be59f6671b70cfb128
maskp <maskpuneet@gmail.com>
2016-12-30 16:11:39 -0500

    video lesson

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -1,45 +1,58 @@
-import React from 'react'
+import React from 'react';
 import Header from './Header';
 import Order from './Order';
-import sampleFishes from '../sample-fishes'
-import AddFishForm from './AddFishForm'
-import Inventory from './Inventory'
-class App extends React.Component{
-	constructor(){
-		super();
-		this.addFish = this.addFish.bind(this)
-		this.loadSamples = this.loadSamples.bind(this)
-		//initial state
-		this.state = {
-			fishes:{},
-			order:{}
-		}
-	}
-	addFish(fish){
-		const fishes = {...this.state.fishes};
-		const timestamp = Date.now();
-		fishes[`fish-${timestamp}`] = fish;
-		this.setstate({fishes});
-	}
+import Inventory from './Inventory';
+import Fish from './Fish';
+import sampleFishes from '../sample-fishes';
 
-	loadSamples(){
-		this.setState({
-			fishes:sampleFishes
-		})
-	}
+class App extends React.Component {
+  constructor() {
+    super();
 
-	render(){
+    this.addFish = this.addFish.bind(this);
+    this.loadSamples = this.loadSamples.bind(this);
 
-		return (
-			<div className="catch-of-the-day">
-				<div className="menu">
-					<Header tagline="fresh sea food market"/>
-				</div>
-				<Order />
-				<Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
-			</div>
-			)
-	}
-	
+    // getinitialState
+    this.state = {
+      fishes: {},
+      order: {}
+    };
+  }
+
+  addFish(fish) {
+    // update our state
+    const fishes = {...this.state.fishes};
+    // add in our new fish
+    const timestamp = Date.now();
+    fishes[`fish-${timestamp}`] = fish;
+    // set state
+    this.setState({ fishes });
+  }
+
+  loadSamples() {
+    this.setState({
+      fishes: sampleFishes
+    });
+  }
+
+  render() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Fresh Seafood Market" />
+          <ul className="list-of-fishes">
+            {
+              Object
+                .keys(this.state.fishes)
+                .map(key => <Fish key={key} details={this.state.fishes[key]} />)
+            }
+          </ul>
+        </div>
+        <Order />
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
+      </div>
+    )
+  }
 }
+
 export default App;
--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import AddFishForm from './AddFishForm'
+import React from 'react';
+import AddFishForm from './AddFishForm';
 
-class Inventory extends React.Component{
-	render() {
-		return (
-			<div>
-				<p>Inventory</p>
-				<AddFishForm addFish={this.props.addFish}/>
-				<button onClick={this.props.loadSamples}>Load SAmaple Fishes</button>
-			</div>
-			)
-	}
+class Inventory extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Inventory</h2>
+        <AddFishForm addFish={this.props.addFish}/>
+        <button onClick={this.props.loadSamples}>Load Sample Fishes</button>
+      </div>
+    )
+  }
 }
 
 export default Inventory;
